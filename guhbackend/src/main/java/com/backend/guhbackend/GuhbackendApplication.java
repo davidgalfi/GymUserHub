c1c3617d0fe860efd8f675ada5e0b1bd629af7cc
davidgalfi <galfidavid314@gmail.com>
2023-08-31 09:24:37 +0200

    # Create Project Structure and Configs
    - Created base project structure.
    - Created maven.pom and application.properties files.
    - Created the main file.

---
+++ guhbackend/src/main/java/com/backend/guhbackend/GuhbackendApplication.java
@@ -0,0 +1,13 @@
+package com.backend.guhbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuhbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuhbackendApplication.class, args);
+	}
+
+}
---
+++ guhbackend/src/test/java/com/backend/guhbackend/GuhbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.guhbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuhbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
