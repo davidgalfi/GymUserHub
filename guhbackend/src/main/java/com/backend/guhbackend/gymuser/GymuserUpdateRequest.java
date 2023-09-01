64b7365bed9cf68fa4039dbb78cdbdb0c5f13374
davidgalfi <galfidavid314@gmail.com>
2023-09-01 16:02:50 +0200

    # Create PUT HTTP route for update user's datas

---
+++ guhbackend/src/main/java/com/backend/guhbackend/gymuser/GymuserUpdateRequest.java
@@ -0,0 +1,11 @@
+package com.backend.guhbackend.gymuser;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+
+public record GymuserUpdateRequest(String name,
+                                   String email,
+                                   LocalDate dob,
+                                   LocalDate registrationDate,
+                                   HashMap<Integer, LocalDate> purchaseDateMap) {
+}
