67293a06bd32615776e512e65f7cb99c325031b5
davidgalfi <galfidavid314@gmail.com>
2023-09-04 17:19:53 +0200

    # Create login request
    - Workers can log in

--- guhbackend/src/test/java/com/backend/guhbackend/GuhbackendApplicationTests.java
+++ guhbackend/src/test/java/com/backend/guhbackend/GuhbackendApplicationTests.java
@@ -1,56 +1,9 @@
 package com.backend.guhbackend;
 
-import com.backend.guhbackend.gymuser.*;
-import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class GuhbackendApplicationTests {
-
-	private final GymuserController gymUserService;
-	private final GymuserRepository gymuserRepository;
-
-	@Autowired
-	GuhbackendApplicationTests(GymuserController gymUserService, GymuserRepository gymuserRepository) {
-		this.gymUserService = gymUserService;
-		this.gymuserRepository = gymuserRepository;
-	}
-
-	@Test
-	void contextLoads() {
-
-	}
-
-	@Test
-	public void testRegisterGymUser() throws GymuserAlreadyRegisteredException {
-		GymuserRegistrationRequest request = new GymuserRegistrationRequest("John", "john@gmail.com", LocalDate.of(1990, 1, 1), LocalDate.now(), new HashMap<>());
-		gymUserService.registerGymUser(request);
-		Optional<Gymuser> gymuserOptional = gymuserRepository.findGymuserByEmail("john@gmail.com");
-		assertTrue(gymuserOptional.isPresent());
-	}
-
-	@Test
-	public void testRegisterGymUserWithAlreadyRegisteredEmail() throws GymuserAlreadyRegisteredException {
-		GymuserRegistrationRequest request1 = new GymuserRegistrationRequest("John", "dave@gmail.com", LocalDate.of(1990, 1, 1), LocalDate.now(), new HashMap<>());
-		gymUserService.registerGymUser(request1);
-		GymuserRegistrationRequest request2 = new GymuserRegistrationRequest("Jane", "dave@gmail.com", LocalDate.of(1995, 1, 1), LocalDate.now(), new HashMap<>());
-
-		GymuserAlreadyRegisteredException exception = assertThrows(GymuserAlreadyRegisteredException.class, () -> gymUserService.registerGymUser(request2));
-		assertTrue(exception.getMessage().contains("Email already registered!"));
-	}
 
 
 }
--- guhfrontend/landingpage/landingcontroller.py
+++ guhfrontend/landingpage/landingcontroller.py
@@ -1,3 +1,4 @@
+import requests
 from flask import Flask, render_template, request, redirect, url_for, Blueprint
 
 landingPage = Blueprint('landingPage', __name__, template_folder='templates', static_folder='static')
@@ -9,9 +10,25 @@ def landing():
     return render_template('landingpage.html')
 
 
-@landingPage.route('/login/')
+@landingPage.route('/login/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        worker_email = request.form.get('worker_email')
+        worker_password = request.form.get('worker_password')
+
+        worker_datas = {
+            'email': worker_email,
+            'password': worker_password
+        }
+
+        response = requests.post('http://localhost:8080/api/gymworker/login', json=worker_datas)
+
+        if response.status_code == 200:
+            return redirect(url_for('workerPage.home'))
+        else:
+            return redirect(url_for('landingPage.login'))
 
 
 @landingPage.route('/register/')
