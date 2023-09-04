37c6bf20a1acc0f188588b480e5c25b57463badc
davidgalfi <galfidavid314@gmail.com>
2023-09-04 12:05:48 +0200

    # Create worker's page

--- guhfrontend/main.py
+++ guhfrontend/main.py
@@ -2,10 +2,11 @@ from flask import Flask, redirect, url_for, render_template, request, jsonify
 import requests
 
 from guhfrontend.landingpage.landingcontroller import landingPage
+from guhfrontend.worker.workercontroller import workerPage
 
 app = Flask(__name__)
 app.register_blueprint(landingPage, url_prefix='/landingPage')
-
+app.register_blueprint(workerPage, url_prefix='/workerPage')
 
 @app.route('/')
 def index():
