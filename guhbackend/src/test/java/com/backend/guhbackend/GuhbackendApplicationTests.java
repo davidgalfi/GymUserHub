35c1e0a8947f030627df6db16b2caf88b4598da1
davidgalfi <galfidavid314@gmail.com>
2023-09-06 10:08:04 +0200

    # Simplify code

--- guhbackend/src/test/java/com/backend/guhbackend/GuhbackendApplicationTests.java
+++ guhbackend/src/test/java/com/backend/guhbackend/GuhbackendApplicationTests.java
@@ -2,8 +2,12 @@ package com.backend.guhbackend;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDate;
+import java.util.LinkedHashMap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class GuhbackendApplicationTests {
-
 
 }
